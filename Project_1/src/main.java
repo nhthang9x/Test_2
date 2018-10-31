70c969885699d4e8e85c3cbd55e0dc7cf7a595e4
thang <thang@DESKTOP-T26I80C>
2018-10-31 16:43:21 +0100

    First Commit

---
+++ Project_1/src/main.java
@@ -0,0 +1,20 @@
+
+public abstract class main {
+
+	public main() {
+		// Nguyen Hung Thang (2nd Semester)
+	}
+
+	@Override
+	public boolean equals(Object arg0) {
+		// TODO Auto-generated method stub
+		return super.equals(arg0);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
